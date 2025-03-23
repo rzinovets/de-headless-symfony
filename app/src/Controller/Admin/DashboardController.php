b9b929f73e93d4f3e1c4151cfdcf7db527a78389
Roman Zinovets <r.zinovets1962@gmail.com>
2025-03-23 22:32:04 +0300

    added translations

--- app/src/Controller/Admin/DashboardController.php
+++ app/src/Controller/Admin/DashboardController.php
@@ -22,11 +22,16 @@ use Cron\CronBundle\Entity\CronJob;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(private readonly TranslatorInterface $translator)
+    {
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
@@ -54,7 +59,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-
         yield MenuItem::section('User Management');
         yield MenuItem::linkToCrud('Users', 'fa fa-user-circle-o', User::class);
         yield MenuItem::linkToCrud('Admin Users', 'fa fa-user-o', Admin::class);
